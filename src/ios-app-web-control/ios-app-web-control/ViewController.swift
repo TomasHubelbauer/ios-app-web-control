0a73c8de1e903b4cb1aa28bebb312a897473c797
Tomas Hubelbauer <Tomas@Hubelbauer.net>
2018-08-24 20:30:23 +0200

    Validate loading HTML into a WebKit view

--- src/ios-app-web-control/ios-app-web-control/ViewController.swift
+++ src/ios-app-web-control/ios-app-web-control/ViewController.swift
@@ -1,7 +1,24 @@
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var contentTextField: UITextField!
+    @IBOutlet weak var controlWebView: WKWebView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        var html = ""
+        html += "<!DOCTYPE html>"
+        html += "<html>"
+        html += "<head>"
+        html += "<title>Control</title>"
+        html += "<meta charset='utf8'>"
+        html += "<meta name='viewport' content='width=device-width' />"
+        html += "</head>"
+        html += "<body>"
+        html += "test"
+        html += "</body>"
+        html += "</html>"
+        controlWebView.loadHTMLString(html, baseURL: nil)
     }
 }
